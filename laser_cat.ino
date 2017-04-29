472e1f2a8e68080bb27cfce096826cd264d8e92f
Zack Michener <zack@zackmichener.net>
2017-04-28 19:39:40 -0700

    update params for new servos; put switch on interrupt

--- laser_cat.ino
+++ laser_cat.ino
@@ -1,28 +1,28 @@
 #include <Servo.h>
 #include <math.h>
 #include <stdlib.h>
-#include "name.h"
 
 // 1/100s sec
 const int SLOW = 200;
-const int FAST = 20;
+const int FAST = 40;
 
 const int SWITCH_PIN = 2;
-const int SWITCH_GND = 3;
+const int GND = 3;
+const int POT_PIN = A5;
 
 // servo angle
-const int X_MOTOR_MIN = 17;
-const int X_MOTOR_MAX = 180;
-const int Y_MOTOR_MIN = 35;
-const int Y_MOTOR_MAX = 180;
+const int X_MOTOR_MIN = 0;
+const int X_MOTOR_MAX = 160;
+const int Y_MOTOR_MIN = 85;
+const int Y_MOTOR_MAX = 250;
 
 // servo angle
-float min_x = 110;
-float max_x = X_MOTOR_MAX;
-float min_y = Y_MOTOR_MIN;
-float max_y = 120;
-float origin_x = 130;
-float origin_y = 35;
+float min_x = 50;
+float max_x = 100;
+float min_y = 85;
+float max_y = 150;
+float origin_x = 64;
+float origin_y = 85;
 float minimal_movement = 5;
 
 // ms
@@ -35,32 +35,38 @@ float y_position = origin_y;
 
 float lSpeed = SLOW;
 
-Agent agent;
-
 Servo x_servo;
 Servo y_servo;
 
 String type = "standard";
-int playTtl = 0;
+volatile int playTtl = 0;
 int laserTtl = 0;
 float movement_time = SLOW;
 bool laserIsOn = false;
 
+float analogReads[10];
+int analogReadsHead = 0;
+
 void setup() {
   Serial.begin(9600);
+  Serial.println("Startup");
   y_servo.attach(9);
   x_servo.attach(6);
   x_servo.write(origin_x);
   y_servo.write(origin_y);
   pinMode (12, OUTPUT); // laser
-  pinMode (SWITCH_GND, OUTPUT);
-  digitalWrite(SWITCH_GND, LOW);
+  pinMode (GND, OUTPUT);
+  digitalWrite(GND, LOW);
   pinMode (SWITCH_PIN, INPUT); // switch
-  reset();
+//  reset();
   laserOn();
+
+  attachInterrupt(digitalPinToInterrupt(SWITCH_PIN), switchInterrupt, LOW);
 }
 
 void loop() {
+  origin_y = analogReadAverage();
+  
   if (Serial.available() > 0) {
     if (Serial.peek() == 'o' || 
         Serial.peek() == 'l' || 
@@ -80,8 +86,10 @@ void loop() {
         playTtl = 100;
         laserOn();
       } else if (c == 'r') {
+        Serial.println("Manual reset");
         reset();
       } else if (c == 'b') {
+        Serial.println("Drawing borders");
         drawBorders();
       }
     } else {
@@ -89,32 +97,18 @@ void loop() {
     }
   }
 
-  int switchVal = digitalRead(SWITCH_PIN);
-  if (switchVal == LOW) {
-    Serial.println("Triggered");
-    if (playTtl > 0) {
-      playTtl = 1;
-    } else {
-      laserOn();
-      playTtl = 100;
-    }
-  }
-
   if (playTtl > 1) {
     playTtl--;
     standard();;
   } else if (playTtl == 1) {
+    Serial.println("Untriggered");
     playTtl--;
     reset();
     laserTtl = 1000000;
+  } else {
+    y_servo.write(origin_y);
   }
 
-  if (laserTtl > 1) {
-    laserTtl--;
-  } else if (laserTtl == 1) {
-    laserTtl--;
-//    laserOff();
-  }
 
 }
 
@@ -128,7 +122,7 @@ void reset() {
 }
 
 void standard() {
-//  float movement_time = random(FAST,SLOW);
+  float movement_time = random(FAST,SLOW);
   lSpeed += random(-5, 5);
   lSpeed = max(min(movement_time, FAST), SLOW);
 
@@ -137,9 +131,9 @@ void standard() {
   float dist = sqrt(pow(newX, 2) + pow(newY, 2));
   float raw_t = dist / lSpeed;
   float normal_t = (raw_t - 0.7) * (1/0.3);
-  float movement_time = SLOW - normal_t * (SLOW - FAST) + FAST;
+//  float movement_time = SLOW - normal_t * (SLOW - FAST) + FAST;
 
-  Serial.println(movement_time);  
+  //Serial.println(movement_time);  
   int random_delay = random(min_freeze, max_freeze);
 
   moveTo(newX, newY, movement_time);
@@ -174,9 +168,9 @@ void moveTo(float x, float y, float t) {
   for(int i = 1; i <= t; i++) {
     cur_x = floor(x_position + i*(x_dist/t));
     cur_y = floor(y_position + i*(y_dist/t));
-    
-    x_servo.write(floor(cur_x));
-    y_servo.write(floor(cur_y));
+
+    x_servo.write(round(cur_x));
+    y_servo.write(round(cur_y));
     delay(10);
   }
 
@@ -222,19 +216,6 @@ float getNewY() {
   return y_new_position;
 }
 
-Vector getNearVector(Vector origin) {
-  float dist = random(minimal_movement*2, minimal_movement*4);
-  float r = static_cast <float>(rand()) / static_cast <float>(RAND_MAX);
-  double dir = origin.direction + 0.1*r - 0.5;
-  float x = constrain(origin.x + dist*cos(dir), min_x, max_x);
-  float y = constrain(origin.y + dist*sin(dir), min_y, max_y);
-  
-  Vector v = Vector { x, y, dir };
-
-  printVector(v);
-  return v;
-}
-
 void laserOn() {
   laserIsOn = true;
   digitalWrite (12, HIGH);
@@ -253,24 +234,26 @@ void toggleLaser() {
   }
 }
 
-void printVector(Vector v) {
-  Serial.print(v.x);
-  Serial.print(", ");
-  Serial.print(v.y);
-  Serial.print(", ");
-  Serial.println(v.direction);
+int analogReadAverage() {
+  float potVal = 0.5*analogRead(POT_PIN);
+  analogReads[analogReadsHead] = potVal;
+  analogReadsHead = (analogReadsHead + 1) % 10;
+
+  float analogSum = 0;
+  for (int i = 0; i < 10; i++) {
+    analogSum += analogReads[i];
+  }
+  int avg = floor(analogSum / 10);
+  return avg;
 }
 
-void sine() {
-  for (double i = -1.0; i <= 1.0; i = i + 0.1) {
-    Serial.println (sin(i));
-    delay(10);
-  }
- 
-  // Describe the downward swing of the sine wave
-  for (double i = 1.0; i >= -1.0; i = i - 0.1) {
-    Serial.println (sin(i));
-    delay(10);
+void switchInterrupt() {
+  if (playTtl > 0 && playTtl < 99) {
+    playTtl = 1;
+  } else if (playTtl == 0) {
+    Serial.println("Triggered");
+    laserOn();
+    playTtl = 100;
   }
 }
 
